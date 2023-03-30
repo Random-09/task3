01c91ac70ba8267fa9454857b30e9447fb161e96
kvasa <kvasan2104@gmail.com>
2023-03-30 17:55:27 +0300

    Added delete and info functions

--- main.c
+++ main.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-
 typedef struct {
     int id;
     char *name;
@@ -25,9 +24,9 @@ int id_uniqueness_check(int id, Student_t *p_database[50]) {
     return 1;
 }
 
-int empty_spot_index(const int count[50]) {                 // <--- change to pointers
+int id_index(int id, Student_t *p_database[50]) {
     for (int i = 0; i < 50; i++) {
-        if (count[i] == 1)
+        if (p_database[i]->id == id)
             return i;
     }
     return -1;
@@ -50,17 +49,40 @@ void add_student(Student_t *p_database[50], int current_index) {
         p_database[current_index]->name = name;
         p_database[current_index]->student_id = student_id;
         p_database[current_index]->average_grade = average_grade;
-    }
-    else
+    } else
         printf("ID is not unique");                        // <---- Test in a while loop
 }
 
-void delete_student(Student_t *p_database[50], int current_index) {
-
+void delete_student(Student_t *p_database[50], int number_of_students) {
+    printf("Enter unique ID of the student");
+    int id;
+    scanf("%d", &id);
+    int index = id_index(id, p_database);
+    if (index != -1) {
+        for (int i = index; i < number_of_students - 1; i++) {
+            p_database[i] = p_database[i + 1];
+        }
+    } else {
+        printf("ID not found in this database");
+        exit(EXIT_FAILURE);                                 // <---- Maybe continue while loop
+    }
 }
 
-void student_info() {
-
+void student_info(Student_t *p_database[50]) {
+    printf("Enter student's id");
+    int id;
+    scanf("%d", &id);
+    int index = id_index(id, p_database);
+    if (index != -1) {
+        char *name = p_database[index]->name;
+        char *student_id = p_database[index]->student_id;
+        float average_grade = p_database[index]->average_grade;
+        printf("Student with id: %d\nName: %s\nStudent id:%s\nAverage grade%f\n",
+               id, name, student_id, average_grade);
+    } else {
+        printf("ID not found in this database");
+        exit(EXIT_FAILURE);                                 // <---- Maybe continue while loop
+    }
 }
 
 void print_average_grades() {
@@ -70,8 +92,7 @@ void print_average_grades() {
 
 int main() {
     int exit_flag = 0;
-    int count[50] = {0};
-    int current_index = 0;
+    int number_of_students = 0;
     Student_t *database[50];
     while (!exit_flag) {
         printf("1. Add student\n2. Delete student\n3. Student info\n4. Print average grades\n5. Exit");
@@ -80,21 +101,27 @@ int main() {
         int choice = strtol(&input, NULL, 10);
         switch (choice) {
             case ADD_STUDENT:
-                add_student(database, current_index);
+                add_student(database, number_of_students);
+                number_of_students++;
             case DELETE_STUDENT:
-                delete_student(database, current_index);
+                delete_student(database, number_of_students);
+                number_of_students--;
             case STUDENT_INFO:
-                student_info();
+                student_info(database);
             case PRINT_AVERAGE_GRADES:
                 print_average_grades();
             case EXIT:
                 exit_flag = 1;
-                break;
             default:
                 printf("Incorrect input");
         }
     }
 }
+
+
+// TODO
+// Проверка на дурака везде, как в main
+
 
 
 // Необходимо реализовать простую базу данных студентов.
